381d2c9909977067edcaeb598cef39728a3918be
ilymil <fryerc@mssm.org>
2026-02-02 08:23:05 -0500

    add arrows to arrowui

--- Scripts/arrow_ui.gd
+++ Scripts/arrow_ui.gd
@@ -4,30 +4,29 @@ var completed_moves = 0;
 
 # sets number of moves contained in this pattern
 # TODO: allow for patterns with directions
-func set_pattern(num_moves : int):
+func set_pattern(directions : Array[Move.Direction]):
 	# reset pattern
 	completed_moves = 0;
 	for n in get_children():
 		n.queue_free();
 		
 	# create new pattern
-	for n in num_moves:
-		var move = ColorRect.new();
-		move.set_color(Color(1, 1, 1, 1));
-		move.custom_minimum_size = Vector2(50, 50);
-		move.update_minimum_size();
-		add_child(move);
+	for d in directions:
+		var note = TextureRect.new();
+		note.texture = load("res://" + Move.getNoteSpriteName(d));
+		note.expand_mode = TextureRect.EXPAND_FIT_WIDTH;
+		add_child(note);
 
 # displays the next move in the pattern as completed
 func increment():
 	if(completed_moves < get_child_count()):
 		var move = get_child(completed_moves);
-		move.set_color(Color(1, 1, 0, 1));
+		move.modulate = Color(1, 0, 0, 1);
 		completed_moves += 1;
 
 # resets completion of displayed moves
 func reset():
 	for i in completed_moves:
 		var move = get_child(i);
-		move.set_color(Color(1, 1, 1, 1));
+		move.modulate = Color(1, 1, 1, 1);
 	completed_moves = 0;
