ccdf8b44259824d29deedd11151a4aab9023ad31
Jordan Serna <145515057+YoshifanJordan2468@users.noreply.github.com>
2026-02-23 09:36:57 -0500

    Implemented basic movelist and choosing a random move

--- Scripts/move_manager.gd
+++ Scripts/move_manager.gd
@@ -1,5 +1,14 @@
 extends Node
 
+var movelist: Array[Move] = [
+	preload("res://Moves/BasicAttack.tres"),
+	preload("res://Moves/Fireball.tres"),
+	preload("res://Moves/MagicMissile.tres"),
+	preload("res://Moves/Rest.tres"),
+	preload("res://Moves/Slash.tres"),
+	preload("res://Moves/Strike.tres")
+]
+
 func addCard(move: Move):
 	if(!gamemanager.movelist.has(move)):
 		gamemanager.movelist.append(move)
@@ -14,3 +23,8 @@ func replaceCard(oldMove: Move, newMove: Move):
 		var value = gamemanager.movelist.find(oldMove)
 		gamemanager.movelist.remove_at(value)
 		gamemanager.movelist.insert(value, newMove)
+
+func chooseRandomMove():
+	var chosenMove = movelist[randi_range(0,movelist.size()-1)]
+	print(chosenMove.name)
+	return chosenMove
