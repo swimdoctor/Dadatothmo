768becda693804a7cd301340aab1c72d8cfe806a
ilymil <fryerc@mssm.org>
2026-03-17 17:10:24 -0400

    add initial new_move function

--- Scripts/MoveList.gd
+++ Scripts/MoveList.gd
@@ -20,9 +20,38 @@ enum MoveName {
 	RESTORE
 }
 
-#func new_move(move : MoveName):
-#	match move:
-#		
+func new_move(move : MoveName):
+	var name : String;
+	var path : String;
+	var pattern : Array[Move.Direction];
+	var method : String;
+	
+	match move:
+		MoveName.STRIKE: 
+			name = "Strike";
+			path = "res://Images/Test/IconRoughSword.png";
+			pattern = [Move.Direction.LEFT, Move.Direction.UP, Move.Direction.DOWN];
+			method = "strike";
+		MoveName.FIREBALL:
+			name = "Fireball";
+			path = "res://Images/Test/IconRoughFire.png";
+			pattern = [Move.Direction.LEFT, Move.Direction.RIGHT, Move.Direction.LEFT, Move.Direction.RIGHT];
+			method = "fireball";
+		MoveName.REST:
+			name = "Rest";
+			path = "res://Images/Test/IconRoughHealth.png";
+			pattern = [Move.Direction.LEFT, Move.Direction.UP, Move.Direction.UP, Move.Direction.UP, Move.Direction.RIGHT];
+			method = "rest";
+		MoveName.MAGIC_MISSILE:
+			name = "Magic Missile";
+			path = "res://Images/Test/IconRoughFire.png"
+			pattern = [Move.Direction.UP, Move.Direction.LEFT, Move.Direction.RIGHT, Move.Direction.UP];
+			method = "magic_missile";
+		MoveName.DOUBLE_ATTACK:
+			name = "Double Attack";
+			path = "res://Images/Test/IconRoughSword.png";
+			pattern = [Move.Direction.UP, Move.Direction.LEFT, Move.Direction.UP, Move.Direction.RIGHT, Move.Direction.DOWN];
+			method = "double_attack";
 
 # deal 0.5x attack stat to first enemy
 func strike(enemies : Array[Enemy], rhythm : Rhythm):
@@ -88,3 +117,66 @@ func moneymaker(enemies : Array[Enemy], rhythm : Rhythm):
 	var target = enemies[0];
 	
 	target.damage(damage * rhythm.attack);
+
+# deal 0.65x attack to lowest health enemy
+func sneak_attack(enemies : Array[Enemy], rhythm : Rhythm):
+	var damage = 0.65;
+	var target = enemies[0];
+	for enemy in enemies:
+		if enemy.health < target.health:
+			target = enemy;
+	
+	target.damage(damage * rhythm.attack);
+
+# deal 0.5x attack and 1.5x attack to second enemy, or 1x attack if none
+func snipe(enemies : Array[Enemy], rhythm : Rhythm):
+	var damage1 = 0.5;
+	var target1 = enemies[0];
+	
+	if enemies.size() > 1:
+		var damage2 = 1.5;
+		var target2 = enemies[1];
+		
+		target2.damage(damage2 * rhythm.attack);
+	else:
+		damage1 = 1;
+	
+	target1.damage(damage1 * rhythm.attack);
+
+# deal 1x attack, 0.05x max health self damage if no kill
+func daylight_job(enemies : Array[Enemy], rhythm : Rhythm):
+	var damage = 1;
+	var target = enemies[0];
+	var self_damage = 0.05;
+	
+	target.damage(damage * rhythm.attack);
+	if target.health > 0:
+		gamemanager.damage_player(self_damage * gamemanager.max_player_health);
+
+# im not even attempting to describe this move here just delete when we get literally any better option
+func last_resort(enemies : Array[Enemy], rhythm : Rhythm):
+	var damage = 1.5;
+	var self_damage = 0.15;
+	var target = enemies[0];
+	for enemy in enemies:
+		if enemy.health < target.health:
+			target = enemy;
+	if target.health < target.max_health * 0.5:
+		damage *= 2;
+		self_damage *= 2;
+	
+	target.damage(damage * rhythm.attack);
+	if target.health > 0:
+		gamemanager.damage_player(self_damage * gamemanager.max_player_health);
+
+# heal 0.1x max health
+func heal(enemies : Array[Enemy], rhythm : Rhythm):
+	var heal = 0.1;
+	
+	gamemanager.heal_player(heal * gamemanager.max_player_health);
+
+# heal 0.2x missing health
+func restore(enemies : Array[Enemy], rhythm : Rhythm):
+	var heal = 0.2;
+	
+	gamemanager.heal_player(heal * gamemanager.max_player_health - gamemanager.player_health);
