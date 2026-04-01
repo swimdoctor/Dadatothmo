1bf72953500f6d4005a126a9fce63be4a05408de
WalrusesAreAwesome <rysdam.jon@gmail.com>
2026-04-01 16:26:16 -0400

    Enemy HP values and light documentation

--- Scripts/EnemyList.gd
+++ Scripts/EnemyList.gd
@@ -22,9 +22,15 @@ func random_enemy() -> EnemyName:
 
 func new_enemy(enemy : EnemyName):
 	var enemy_name : String;
-	var description : String
-	var path : String
+	var description : String;
+	
+	## File path to the enemy's sprite
+	var path : String;
+	
+	## Amount of health the enemy spawns with
 	var max_health : int;
+	
+	## Enemy will perform attacks in this order
 	var attack_pattern : Array[EnemyMove];
 	
 	match enemy:
@@ -39,49 +45,49 @@ func new_enemy(enemy : EnemyName):
 			enemy_name = "Bardic Boys";
 			description = "They've got each other's backs";
 			path = "res://Images/Test/IconRoughSword.png";
-			max_health = 100;
+			max_health = 200;
 			attack_pattern = [];
 			return;
 		EnemyName.FROGLINSPLODE:
 			enemy_name = "His Royal Magesty, Froginsplode";
 			description = "King of the goblin sect. Wields explosives.";
 			path = "res://Images/Test/IconRoughSword.png";
-			max_health = 100;
+			max_health = 500;
 			attack_pattern = [];
 			return;
 		EnemyName.ROADIE:
 			enemy_name = "Roadie";
 			description = "Not the musical type.";
 			path = "res://Images/Test/IconRoughSword.png";
-			max_health = 100;
+			max_health = 150;
 			attack_pattern = [];
 			return;
 		EnemyName.GREG_TEMPLE:
 			enemy_name = "Greg Temple";
 			description = "The Temple name is renowned throughout the Underground.";
 			path = "res://Images/Test/IconRoughSword.png";
-			max_health = 100;
+			max_health = 600;
 			attack_pattern = [];
 			return;
 		EnemyName.MIKE_WIRE:
 			enemy_name = "Mike Wire";
 			description = "Leader of the Choir";
 			path = "res://Images/Test/IconRoughSword.png";
-			max_health = 100;
+			max_health = 80;
 			attack_pattern = [];
 			return;
 		EnemyName.TODD_BEARSOOS:
 			enemy_name = "Todd Bearsoos";
 			description = "Wants to make you sledepy";
 			path = "res://Images/Test/IconRoughSword.png";
-			max_health = 100;
+			max_health = 150;
 			attack_pattern = [];
 			return;
 		EnemyName.ICE_SPIDER:
 			enemy_name = "Ice Spider";
 			description = "ICE. SPIDER.";
 			path = "res://Images/Test/IconRoughSword.png";
-			max_health = 100;
+			max_health = 300;
 			attack_pattern = [];
 			return;
 			
