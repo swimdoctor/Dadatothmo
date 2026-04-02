553b144e89e60ac7af5a157dd02670055a6bc226
Dylan Shaffer <swimdoctor5@gmail.com>
2026-04-02 03:39:33 -0400

    Imported images and forgot to pull (sobbing)

--- Scripts/EnemyList.gd
+++ Scripts/EnemyList.gd
@@ -18,78 +18,71 @@ func set_enemy(enemy_name : EnemyName) -> void:
 	#Later need to support multi-enemy combats
 
 func random_enemy() -> EnemyName:
-	return randi() % EnemyName.size();
+	return randi() % EnemyName.size()
+
+func random_enemy_data() -> EnemyData:
+	return new_enemy(random_enemy())
 
 func new_enemy(enemy : EnemyName):
 	var enemy_name : String;
 	var description : String;
 	
 	## File path to the enemy's sprite
-	var path : String;
+	var sprite_path : String;
 	
 	## Amount of health the enemy spawns with
 	var max_health : int;
 	
 	## Enemy will perform attacks in this order
 	var attack_pattern : Array[EnemyMove];
-	
+
 	match enemy:
 		EnemyName.GOBLIN:
-			enemy_name = "Golbin the Gobbin";
-			description = "Can strike";
-			path = "res://Images/Test/IconRoughSword.png";
-			max_health = 100;
-			attack_pattern = [enemymovelist.new_enemy_move(enemymovelist.EnemyMoveName.STRIKE)];
-			return;
+			enemy_name = "Golbin the Gobbin"
+			description = "Can strike"
+			sprite_path = "res://Images/Test/IconRoughSword.png"
+			max_health = 100
+			attack_pattern = [enemymovelist.new_enemy_move(EnemyMoveList.EnemyMoveName.STRIKE)]
 		EnemyName.BARDIC_BOYS:
-			enemy_name = "Bardic Boys";
-			description = "They've got each other's backs";
-			path = "res://Images/Test/IconRoughSword.png";
-			max_health = 200;
-			attack_pattern = [];
-			return;
+			enemy_name = "Bardic Boys"
+			description = "They've got each other's backs"
+			sprite_path = "res://Images/Test/Enemies/BardBoyMarisa.png"
+			max_health = 200
 		EnemyName.FROGLINSPLODE:
-			enemy_name = "His Royal Magesty, Froginsplode";
-			description = "King of the goblin sect. Wields explosives.";
-			path = "res://Images/Test/IconRoughSword.png";
-			max_health = 500;
-			attack_pattern = [];
-			return;
+			enemy_name = "His Royal Magesty, Froginsplode"
+			description = "King of the goblin sect. Wields explosives."
+			sprite_path = "res://Images/Test/Froglinsplode.png"
+			max_health = 500
 		EnemyName.ROADIE:
-			enemy_name = "Roadie";
-			description = "Not the musical type.";
-			path = "res://Images/Test/IconRoughSword.png";
-			max_health = 150;
-			attack_pattern = [];
-			return;
+			enemy_name = "Roadie"
+			description = "Not the musical type."
+			sprite_path = "res://Images/Test/Roadie.png"
+			max_health = 150
 		EnemyName.GREG_TEMPLE:
-			enemy_name = "Greg Temple";
-			description = "The Temple name is renowned throughout the Underground.";
-			path = "res://Images/Test/IconRoughSword.png";
-			max_health = 600;
-			attack_pattern = [];
-			return;
+			enemy_name = "Greg Temple"
+			description = "The Temple name is renowned throughout the Underground."
+			sprite_path = "res://Images/Test/GregTemple.png"
+			max_health = 600
 		EnemyName.MIKE_WIRE:
-			enemy_name = "Mike Wire";
-			description = "Leader of the Choir";
-			path = "res://Images/Test/IconRoughSword.png";
-			max_health = 80;
-			attack_pattern = [];
-			return;
+			enemy_name = "Mike Wire"
+			description = "Leader of the Choir"
+			sprite_path = "res://Images/Test/MikeWire.png"
+			max_health = 80
 		EnemyName.TODD_BEARSOOS:
-			enemy_name = "Todd Bearsoos";
-			description = "Wants to make you sledepy";
-			path = "res://Images/Test/IconRoughSword.png";
-			max_health = 150;
-			attack_pattern = [];
-			return;
+			enemy_name = "Todd Bearsoos"
+			description = "Wants to make you sledepy"
+			sprite_path = "res://Images/Test/ToddBearsoos.png"
+			max_health = 150
 		EnemyName.ICE_SPIDER:
-			enemy_name = "Ice Spider";
-			description = "ICE. SPIDER.";
-			path = "res://Images/Test/IconRoughSword.png";
-			max_health = 300;
-			attack_pattern = [];
-			return;
-			
-	
-	return #Enemy.new(enemy, enemy_name, description, load(path), max_health, attack_pattern);
+			enemy_name = "Ice Spider"
+			description = "ICE. SPIDER."
+			sprite_path = "res://Images/Test/IceSpider.png"
+			max_health = 300
+
+	var data = EnemyData.new()
+	data.enemy_name = enemy_name
+	data.description = description
+	data.sprite_path = sprite_path
+	data.max_health = max_health
+	data.attack_pattern = attack_pattern
+	return data
---
+++ Scripts/Resources/EnemyData.gd
@@ -0,0 +1,8 @@
+class_name EnemyData
+extends Resource
+
+var enemy_name: String = ""
+var description: String = ""
+var sprite_path: String = ""
+var max_health: int = 0
+var attack_pattern: Array[EnemyMove] = []
